4d66aed5f2993c0fa74d4f761d017260728cf102
caincdiy <43126828+caincdiy@users.noreply.github.com>
2019-03-24 21:59:13 -0700

    different response

--- generateResponse.py
+++ generateResponse.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jan 30 14:03:35 2019
 
-@author: whq672437089
+@author: whq672437089 yue cao59468165
 """
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,30 +11,34 @@ import numpy as np
 import random as ran
 from nltk.stem import WordNetLemmatizer
 
-#for a given sentence,return a lemmatized sentence
+
+# for a given sentence,return a lemmatized sentence
+
 def lemTokens(tokens):
-    lemmatizer=WordNetLemmatizer()
+    lemmatizer = WordNetLemmatizer()
     return [lemmatizer.lemmatize(token) for token in tokens]
 
-def generateResponse(userInput,sentences,askResponseDict,ql,similarityThredhold=0.7):
-    #prevent bad input
-    if ((similarityThredhold>1) or (similarityThredhold<0)):
-        similarityThredhold=0.5
+
+def generateResponse(userInput, sentences, askResponseDict, ql, similarityThredhold=0.7):
+    # prevent bad input
+    if ((similarityThredhold > 1) or (similarityThredhold < 0)):
+        similarityThredhold = 0.5
     sentences.append(userInput)
-    #vetorize sentences and userinput for fllowing similarity calculation
-    vertorizedSentences=TfidfVectorizer(tokenizer=lemTokens,stop_words='english').fit_transform(sentences)
+    # vetorize sentences and userinput for fllowing similarity calculation
+    vertorizedSentences = TfidfVectorizer(tokenizer=lemTokens, stop_words='english').fit_transform(sentences)
     vals = cosine_similarity(vertorizedSentences[-1], vertorizedSentences)
-    #find index of sentences that has highest similarity with input
-    valsWithoutLast=vals[0,:-1]
-    idx=np.argmax(valsWithoutLast,axis=0)
-    #return response
-    if(vals[0][idx]<similarityThredhold):
-        response=["Your input keywords donot exist in my knowledge","That's a good question ","Sorry, I don't understand","I don't rightly know","I don't have that in my program"]
-        r=ran.randint(0,4)
-        robotResponse=response[r]
+    # find index of sentences that has highest similarity with input
+    valsWithoutLast = vals[0, :-1]
+    idx = np.argmax(valsWithoutLast, axis=0)
+    # return response
+    if (vals[0][idx] < similarityThredhold):
+        response = ["Your input keywords donot exist in my knowledge", "That's a good question ",
+                    "Sorry, I don't understand", "I don't rightly know", "I don't have that in my program"]
+        r = ran.randint(0, 4)
+        robotResponse = response[r]
         return robotResponse
     else:
-        question=ql[idx]
-        print("matched question from database: "+question)
-        robotResponse =''+askResponseDict.get(question)
+        question = ql[idx]
+        print("matched question from database: " + question)
+        robotResponse = '' + askResponseDict.get(question)
         return robotResponse
