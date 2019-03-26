6c55a53fe9d8165a57951aa4e7c838c9da3b6c7e
caincdiy <43126828+caincdiy@users.noreply.github.com>
2019-03-25 18:06:22 -0700

    Client and server

---
+++ Client.py
@@ -0,0 +1,59 @@
+#author: yue cao 59468165
+
+import socket
+from tkinter import *
+import time
+s = socket.socket()
+s.connect(('127.0.0.1',9000))
+
+def sendmessage():
+    mes=getline()
+    content = 'me ' + time.strftime('%Y-%M-%D %H:%M:%S', time.localtime()) + '\n'
+    text_list.insert(END, content, 'sendcolor')
+    text_list.insert(END, mes)
+    s.sendall(mes.encode("utf-8"))
+    rep=s.recv(2048).decode("utf-8")
+    if rep=='bye':
+        getmessage('bye')
+        s.close()
+        sys.exit()
+    getmessage(rep)
+    text_sent.delete('0.0', END)
+
+def getmessage(replay):
+    content = 'chatbot ' + time.strftime('%Y-%M-%D %H:%M:%S', time.localtime()) + '\n'
+    text_list.insert(END, content, 'rescolor')
+    text_list.insert(END, replay)
+    text_list.see(END)
+
+def getline():
+    mes = text_sent.get('0.0', END)
+    #text_sent.delete('0.0', END)
+    return mes
+
+def enter(event=None):
+    sendmessage()
+
+
+root = Tk()
+root.title("chatbot")
+root.geometry("500x500")
+frm_output = Frame(root, width=500, height=300)
+frm_input = Frame(root, width=500, height=150)
+frm_button = Frame(root, width=500, height=50)
+text_list = Text(frm_output)
+text_list.tag_configure('sendcolor', foreground='green')
+text_list.tag_configure('rescolor', foreground='blue')
+text_sent = Text(frm_input)
+send = Button(frm_button, text='Send', font=10, command=getmessage)
+text_sent.bind('<Return>', enter)
+frm_output.propagate(0)
+frm_output.grid(row=0, column=0)
+frm_input.propagate(0)
+frm_input.grid(row=1, column=0)
+frm_button.propagate(0)
+frm_button.grid(row=2, column=0)
+send.pack()
+text_list.pack()
+text_sent.pack()
+root.mainloop()
---
+++ server.py
@@ -0,0 +1,24 @@
+#author: yue cao 59468165
+
+import socket
+import sys
+import robot as ro
+
+sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host = '127.0.0.1'
+port =9000
+sk.bind((host,port))
+sk.listen(1)
+while True:
+    clnt,addr =sk.accept()
+    while True:
+        data = clnt.recv(1024)
+        if not data:
+             sys.exit()
+        replay=ro.res(data.decode("utf-8"))
+
+        clnt.sendall(replay.encode("utf-8"))
+clnt.close()
+
+
+
